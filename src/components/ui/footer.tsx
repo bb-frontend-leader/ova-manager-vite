0b395185b818548acbb8d8a692a9958bf75c864d
bb-frontend-leader <coor_frontend@booksandbooksdigital.com.co>
2026-03-26 16:36:36 -0500

    fix: update footer background color from white to custom color

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="border-t-2 bg-white border-border rounded-none px-3 py-0.5 text-center text-sm">
+    <footer className="border-t-2 bg-bw border-border rounded-none px-3 py-0.5 text-center text-sm">
       Made with 💛 at Books&Books
     </footer>
   );
