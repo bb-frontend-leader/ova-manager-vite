1b048cfefad910bada87125255460c3c6930e67f
bb-frontend-leader <coor_frontend@booksandbooksdigital.com.co>
2026-03-26 16:35:45 -0500

    fix: update footer heart color from purple to yellow

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="border-t-2 bg-white border-border rounded-none px-3 py-0.5 text-center text-sm">
-      Made with 💜 at Books&Books
+      Made with 💛 at Books&Books
     </footer>
   );
 };
